6e7e8122ae93a1303b03556c8709004c1bfa9a42
serg-z <zhur.git@gmail.com>
2014-11-15 12:36:51 +0600

    Add table with text

--- src/com/Chris/NetTool/NetTool.java
+++ src/com/Chris/NetTool/NetTool.java
@@ -5,6 +5,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
 import android.util.Log;
 
 public class NetTool extends Activity {
@@ -24,11 +30,56 @@ public class NetTool extends Activity {
         Log.d(TAG, "tick");
     }
 
+    void addRow(TableLayout tl, String label, int id) {
+        TableRow tr = new TableRow(this);
+
+        tl.addView(tr);
+
+        TextView tv0 = new TextView(this);
+        TextView tv1 = new TextView(this);
+
+        tv0.setText(label);
+        tv1.setId(id);
+
+        final int p = 5;
+
+        tv0.setPadding(p, 0, p, 0);
+        tv1.setPadding(p, 0, p, 0);
+
+        tr.addView(tv0);
+        tr.addView(tv1);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Log.d(TAG, "Created");
+
+        // create ui
+
+        LinearLayout layout = new LinearLayout(this);
+
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+
+        setContentView(layout);
+
+        TableLayout tableLayout = new TableLayout(this);
+
+        layout.addView(tableLayout);
+
+        addRow(tableLayout, "MAC", R.id.text_mac);
+        addRow(tableLayout, "Local IP", R.id.text_local_ip);
+        addRow(tableLayout, "SSID", R.id.text_ssid);
+        addRow(tableLayout, "BSSID", R.id.text_bssid);
+        addRow(tableLayout, "DNS 1", R.id.text_dns1);
+        addRow(tableLayout, "DNS 2", R.id.text_dns2);
+        addRow(tableLayout, "Gateway", R.id.text_gateway);
+        addRow(tableLayout, "Netmask", R.id.text_netmask);
+        addRow(tableLayout, "Server Address", R.id.text_server_address);
+        addRow(tableLayout, "RSSI", R.id.text_rssi);
+        addRow(tableLayout, "Link Speed", R.id.text_link_speed);
     }
 
     @Override
