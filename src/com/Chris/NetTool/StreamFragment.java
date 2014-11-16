0a9f8c387ef081f3536fe1cf70d979f2928a23c7
serg-z <zhur.git@gmail.com>
2014-11-17 01:59:49 +0600

    Add video view and controls to stream page
    Controls include: video address text edit, play, pause, stop buttons and (disabled) random seek button and repeat check box

--- src/com/Chris/NetTool/StreamFragment.java
+++ src/com/Chris/NetTool/StreamFragment.java
@@ -5,17 +5,28 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.VideoView;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 
+import android.net.Uri;
+
 import android.support.v4.app.Fragment;
 
-public class StreamFragment extends Fragment {
+import android.util.Log;
+
+public class StreamFragment extends Fragment implements View.OnClickListener {
     Activity mActivity;
+    VideoView mVideoView;
+    EditText mVideoAddress;
+    Button mButtonPlay, mButtonPause, mButtonStop, mButtonRandomSeek;
+    boolean mVideoIsPaused = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,14 +39,105 @@ public class StreamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         LinearLayout layout = new LinearLayout(mActivity);
 
+        layout.setOrientation(LinearLayout.VERTICAL);
         layout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
-        TextView tv = new TextView(mActivity);
+        mVideoAddress = new EditText(mActivity);
 
-        layout.addView(tv);
+        mVideoAddress.setText("http://");
 
-        tv.setText("Stream");
+        layout.addView(mVideoAddress);
+
+        mVideoView = new VideoView(mActivity);
+
+        mVideoView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 300));
+
+        layout.addView(mVideoView);
+
+        mVideoView.setVisibility(View.VISIBLE);
+
+        // control buttons
+
+        LinearLayout layoutH = new LinearLayout(mActivity);
+
+        layoutH.setOrientation(LinearLayout.HORIZONTAL);
+        layoutH.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+
+        layout.addView(layoutH);
+
+        mButtonPlay = new Button(mActivity);
+
+        layoutH.addView(mButtonPlay);
+
+        mButtonPlay.setText("Play");
+
+        mButtonPlay.setOnClickListener(this);
+
+        mButtonPause = new Button(mActivity);
+
+        layoutH.addView(mButtonPause);
+
+        mButtonPause.setText("Pause");
+
+        mButtonPause.setOnClickListener(this);
+
+        mButtonStop = new Button(mActivity);
+
+        layoutH.addView(mButtonStop);
+
+        mButtonStop.setText("Stop");
+
+        mButtonStop.setOnClickListener(this);
+
+        CheckBox checkBoxRepeat = new CheckBox(mActivity);
+
+        layoutH.addView(checkBoxRepeat);
+
+        checkBoxRepeat.setText("Repeat");
+
+        checkBoxRepeat.setEnabled(false);
+
+        //
+
+        layoutH = new LinearLayout(mActivity);
+
+        layoutH.setOrientation(LinearLayout.HORIZONTAL);
+        layoutH.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+
+        layout.addView(layoutH);
+
+        mButtonRandomSeek = new Button(mActivity);
+
+        mButtonRandomSeek.setOnClickListener(this);
+
+        layoutH.addView(mButtonRandomSeek);
+
+        mButtonRandomSeek.setText("Random Seek");
+
+        mButtonRandomSeek.setEnabled(false);
 
         return layout;
+    }
+
+    public void onClick(View view) {
+        if (view == mButtonPlay) {
+            if (!mVideoIsPaused) {
+                mVideoView.setVideoURI(Uri.parse(mVideoAddress.getText().toString()));
+            }
+
+            mVideoView.start();
+
+            mVideoIsPaused = false;
+        } else if (view == mButtonPause) {
+            mVideoView.pause();
+
+            mVideoIsPaused = true;
+        } else if (view == mButtonStop) {
+            mVideoView.stopPlayback();
+
+            mVideoIsPaused = false;
+        } else if (view == mButtonRandomSeek) {
+            mVideoView.seekTo(0);
+        }
     }
 }
