e587be2e5b2cc5b58dbc6287063fd15c1c99dc8a
serg-z <zhur.git@gmail.com>
2015-01-18 22:53:11 +0600

    Add "message_id" parameter to "broadcast_to_peds" script
    This parameter is taken from second argument and used to distinguish messages on devices

--- datagram_utils/broadcast_to_peds.sh
+++ datagram_utils/broadcast_to_peds.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+MESSAGE_ID="$2"
+
+if [ -z "$MESSAGE_ID" ]; then
+    MESSAGE_ID="-1"
+fi
+
 CONFIG="`cat $1 \
     | sed '/^#.*$/d' \
     | sed '/^$/d'`"
 
 echo "Sending $1 (bytes after filtering: $(echo \"$CONFIG\" | wc -c))"
 
-echo "$CONFIG" | socat -uv - UDP-DATAGRAM:255.255.255.255:55555,broadcast
+echo -e "message_id=$MESSAGE_ID\n$CONFIG" \
+    | socat -uv - UDP-DATAGRAM:255.255.255.255:55555,broadcast
